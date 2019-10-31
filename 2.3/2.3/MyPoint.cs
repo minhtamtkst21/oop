beb55e4113acc827ae53cd8c9e48ebf095ee376f
TienDat <tiendat.tr158@gmail.com>
2019-10-31 18:00:43 +0700

    oop

---
+++ 2.3/2.3/MyPoint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3
+{
+    class MyPoint
+    {
+        private int X;
+        private int Y;
+        public MyPoint(int x = 0, int y = 0)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public int getX() { return this.X; }
+        public int getY() { return this.Y; }
+        public void setX(int x) { this.X = x; }
+        public void setY(int y) { this.Y = y; }
+        public int[] getXY() 
+        {
+            int[] xy = new int[2];
+            xy[0] = this.X;
+            xy[1] = this.Y;
+            return xy;
+        }
+        public void setXY(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", this.X, this.Y);
+        }
+        public double Distance(int x, int y)
+        {
+            int deltaX = this.X - x;
+            int deltaY = this.Y - y;
+            return Math.Sqrt(Math.Abs(deltaX*deltaX+deltaY*deltaY));
+        }
+        public double Distance(MyPoint point)
+        {
+            return Distance(point.X, point.Y);
+        }
+        public double Distance()
+        {
+            return Distance(0, 0);
+        }
+    }
+}
---
+++ 2.3/2.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyPoint p1 = new MyPoint();  // Test constructor
+            Console.WriteLine(p1);      // Test toString()
+            p1.setX(8);   // Test setters
+            p1.setY(6);
+            Console.WriteLine("x is: " + p1.getX());  // Test getters
+            Console.WriteLine("y is: " + p1.getY());
+            p1.setXY(3, 0);   // Test setXY()
+            Console.WriteLine(p1.getXY()[0]);  // Test getXY()
+            Console.WriteLine(p1.getXY()[1]);
+            Console.WriteLine(p1);
+
+            MyPoint p2 = new MyPoint(0, 4);  // Test another constructor
+            Console.WriteLine(p2);
+            // Testing the overloaded methods distance()
+            Console.WriteLine(p1.Distance(p2));    // which version?
+            Console.WriteLine(p2.Distance(p1));    // which version?
+            Console.WriteLine(p1.Distance(5, 6));  // which version?
+            Console.WriteLine(p1.Distance());  
+        }
+    }
+}
